f1f824e7ef362ee5fa56349998abd7bc8a401c2c
Zach <dland82995@protonmail.com>
2020-04-21 22:50:19 -0500

    First commit

---
+++ src/main/java/com/hazyarc14/BronzeLeagueBotApplication.java
@@ -0,0 +1,13 @@
+package com.hazyarc14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BronzeLeagueBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BronzeLeagueBotApplication.class, args);
+	}
+
+}
